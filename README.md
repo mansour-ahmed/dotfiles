cd4f26e2c5ace72ee6f3c2e8c4c5b5b0f1b91281
Ahmed Mansour <ahmed@mansour.fi>
2020-04-25 20:25:22 +0300

    Init brew packages

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# dotfiles
+
+# Dotfiles &middot; [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](https://github.com/your/your-project/blob/master/LICENSE)
+
+Personal dotfiles to sync MacOS configurations across macbooks.
+
+## Installing / Getting started
+
+```shell
+    curl --silent https://raw.githubusercontent.com/mansour-ahmed/dotfiles/master/bootstrap.sh | bash
+```
+
---
+++ bootstrap.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+
+DOTFILE_REPO_GITHUB_LINK=https://raw.githubusercontent.com/mansour-ahmed/dotfiles/master
+DOTFILES_REPO=~/Desktop/dotfiles
+
+main() {
+    # Required to install packages
+    ask_for_sudo
+    # To manage packages
+    install_xcode_command_line_tools
+    clone_dotfiles_repo
+    install_homebrew
+    install_packages_with_brewfile
+}
+
+function ask_for_sudo() {
+    info "Prompting for sudo password"
+    if sudo --validate; then
+        # Keep-alive
+        while true; do sudo --non-interactive true; \
+            sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+        success "Sudo credentials updated"
+    else
+        error "Obtaining sudo credentials failed"
+        exit 1
+    fi
+}
+
+function install_xcode_command_line_tools() {
+    info "Installing Xcode command line tools"
+
+    if softwareupdate --history | grep --silent "Command Line Tools"; then
+        success "Xcode command line tools already exists"
+    else
+        xcode-select --install
+        while true; do
+            if softwareupdate --history | grep --silent "Command Line Tools"; then
+                success "Xcode command line tools installation succeeded"
+                break
+            else
+                substep "Xcode command line tools still installing..."
+                sleep 20
+            fi
+        done
+    fi
+}
+
+function clone_dotfiles_repo() {
+    info "Cloning dotfiles repository into ${DOTFILES_REPO}"
+    if test -e $DOTFILES_REPO; then
+        substep "${DOTFILES_REPO} already exists"
+        pull_latest $DOTFILES_REPO
+        success "Pull successful in ${DOTFILES_REPO} repository"
+    else
+        url=https://github.com/mansour-ahmed/dotfiles.git
+        if git clone "$url" $DOTFILES_REPO; then
+            success "Cloned into ${DOTFILES_REPO}"
+        else
+            error "Cloning into ${DOTFILES_REPO} failed"
+            exit 1
+        fi
+    fi
+}
+
+
+function install_homebrew() {
+    info "Installing Homebrew"
+    if hash brew 2>/dev/null; then
+        success "Homebrew already exists"
+    else
+        url=https://raw.githubusercontent.com/Homebrew/install/master/install
+        if yes | /usr/bin/ruby -e "$(curl -fsSL ${url})"; then
+            success "Homebrew installation succeeded"
+        else
+            error "Homebrew installation failed"
+            exit 1
+        fi
+    fi
+}
+
+function install_packages_with_brewfile() {
+    info "Installing Brewfile packages"
+
+    TAP=${DOTFILES_REPO}/brew/Brewfile_tap
+    BREW=${DOTFILES_REPO}/brew/Brewfile_brew
+    CASK=${DOTFILES_REPO}/brew/Brewfile_cask
+    MAS=${DOTFILES_REPO}/brew/Brewfile_mas
+
+    if hash parallel 2>/dev/null; then
+        substep "parallel already exists"
+    else
+        if brew install parallel &> /dev/null; then
+            printf 'will cite' | parallel --citation &> /dev/null
+            substep "parallel installation succeeded"
+        else
+            error "parallel installation failed"
+            exit 1
+        fi
+    fi
+
+    if (echo $TAP; echo $BREW; echo $CASK; echo $MAS) | parallel --verbose --linebuffer -j 4 brew bundle check --file={} &> /dev/null; then
+        success "Brewfile packages are already installed"
+    else
+        if brew bundle --file="$TAP"; then
+            substep "Brewfile_tap installation succeeded"
+
+            export HOMEBREW_CASK_OPTS="--no-quarantine"
+            if (echo $BREW; echo $CASK; echo $MAS) | parallel --verbose --linebuffer -j 3 brew bundle --file={}; then
+                success "Brewfile packages installation succeeded"
+            else
+                error "Brewfile packages installation failed"
+                exit 1
+            fi
+        else
+            error "Brewfile_tap installation failed"
+            exit 1
+        fi
+    fi
+}
+
+function pull_latest() {
+    substep "Pulling latest changes in ${1} repository"
+    if git -C $1 pull origin master &> /dev/null; then
+        return
+    else
+        error "Please pull the latest changes in ${1} repository manually"
+    fi
+}
+
+
+# Utils
+
+function coloredEcho() {
+    local exp="$1";
+    local color="$2";
+    local arrow="$3";
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+        black) color=0 ;;
+        red) color=1 ;;
+        green) color=2 ;;
+        yellow) color=3 ;;
+        blue) color=4 ;;
+        magenta) color=5 ;;
+        cyan) color=6 ;;
+        white|*) color=7 ;; # white or invalid color
+       esac
+    fi
+    tput bold;
+    tput setaf "$color";
+    echo "$arrow $exp";
+    tput sgr0;
+}
+
+function info() {
+    coloredEcho "$1" blue "========>"
+}
+
+function substep() {
+    coloredEcho "$1" magenta "===="
+}
+
+function success() {
+    coloredEcho "$1" green "========>"
+}
+
+function error() {
+    coloredEcho "$1" red "========>"
+}
+
+
+main "$@"
