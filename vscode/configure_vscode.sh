9c6b7464eca0bcbfe243caa6d32994d2a1731a6f
Ahmed Mansour <mail+git@mansour.fi>
2021-12-14 19:15:55 +0200

    Update vscode settings & extensions to latest

--- vscode/configure_vscode.sh
+++ vscode/configure_vscode.sh
@@ -10,53 +10,31 @@ main() {
 function install_extensions() {
     code -v > /dev/null
     if [[ $? -eq 0 ]];then
-        # Javascript
-        code --install-extension dsznajder.es7-react-js-snippets    
-        code --install-extension eg2.tslint
-        code --install-extension dbaeumer.vscode-eslint
-        code --install-extension esbenp.prettier-vscode
-        code --install-extension Orta.vscode-jest
-        code --install-extension CoenraadS.bracket-pair-colorizer
-        code --install-extension whtouche.vscode-js-console-utils
-        code --install-extension chakrounanas.turbo-console-log
-        code --install-extension orta.vscode-jest
-        # Typescript
-        code --install-extension miclo.sort-typescript-imports
-        # Env
-        code --install-extension mikestead.dotenv
-        code --install-extension EditorConfig.EditorConfig
-        # Chrome debugger
-        code --install-extension msjsdiag.debugger-for-chrome
-        # HTML
+        code --install-extension amatiasq.sort-imports
         code --install-extension bradgashler.htmltagwrap
+        code --install-extension ChakrounAnas.turbo-console-log
+        code --install-extension christian-kohler.npm-intellisense
+        code --install-extension CoenraadS.bracket-pair-colorizer
+        code --install-extension cssho.vscode-svgviewer
+        code --install-extension dbaeumer.vscode-eslint
+        code --install-extension deerawan.vscode-dash
+        code --install-extension donjayamanne.githistory
+        code --install-extension dsznajder.es7-react-js-snippets
+        code --install-extension eamodio.gitlens
+        code --install-extension EditorConfig.EditorConfig
+        code --install-extension esbenp.prettier-vscode
         code --install-extension formulahendry.auto-close-tag
         code --install-extension formulahendry.auto-rename-tag
-        # Utils
-        code --install-extension christian-kohler.npm-intellisense
-        code --install-extension streetsidesoftware.code-spell-checker
-        code --install-extension cssho.vscode-svgviewer
-        code --install-extension steoates.autoimport
-        code --install-extension donjayamanne.githistory
-        code --install-extension eamodio.gitlens
-        code --install-extension zainchen.json
+        code --install-extension miclo.sort-typescript-imports
+        code --install-extension mikestead.dotenv
+        code --install-extension Orta.vscode-jest
         code --install-extension pflannery.vscode-versionlens
-        code --install-extension coenraads.bracket-pair-colorizer
-        code --install-extension editorconfig.editorconfig
+        code --install-extension steoates.autoimport
+        code --install-extension streetsidesoftware.code-spell-checker
+        code --install-extension stylelint.vscode-stylelint
+        code --install-extension whtouche.vscode-js-console-utils
         code --install-extension yzhang.markdown-all-in-one
-        # Theme
-        code --install-extension johnpapa.winteriscoming
-
-        ## TEMP disabled
-        # code --install-extension jpoissonnier.vscode-styled-components
-
-        # code --install-extension PeterJausovec.vscode-docker
-
-        # code --install-extension ms-vsliveshare.vsliveshare
-
-        # code --instal-extension hashicorp.terraform
-
-        # code --install-extension sdras.vue-vscode-snippets
-        # code --install-extension octref.vetur
+        code --install-extension ZainChen.json
         fi
 }
 
