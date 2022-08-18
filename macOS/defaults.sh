1cf526cfeff2de512d83ea08f6ac1470160c26a9
mbpro16m1max <mbpro16m1max@TheOne.local>
2022-08-18 13:12:11 +0300

    Add macos security defaults

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -60,6 +60,9 @@ function configure_system() {
 
     # Set the timezone; see 'sudo systemsetup -listtimezones' for other values
     sudo systemsetup -settimezone "Europe/Helsinki" > /dev/null
+
+    # Disable crash reporter (the dialog which appears after an application crashes and prompts to report the problem to Apple)
+    defaults write com.apple.CrashReporter DialogType none
 }
 
 function configure_dock() {
@@ -160,6 +163,10 @@ function configure_finder() {
     # Avoid creating .DS_Store files on network or USB volumes
     defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
     defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+    # Expose hidden files and Library folder in Finder:
+    defaults write com.apple.finder AppleShowAllFiles -bool true
+    chflags nohidden ~/Library
 }
 
 function configure_app_store() {
@@ -184,6 +191,385 @@ function configure_3rd_party () {
     touch ~/Library/Application\ Support/Spotify/prefs
     echo 'app.autostart-mode="off"' >> ~/Library/Application\ Support/Spotify/prefs
     echo 'app.autostart-banner-seen=true' >> ~/Library/Application\ Support/Spotify/prefs
+}
+
+function security () {
+sudo scutil --set ComputerName 💻
+sudo scutil --set LocalHostName TheOne
+
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned off
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
+
+rm -rfv "~/Library/LanguageModeling/*" "~/Library/Spelling/*" "~/Library/Suggestions/*"
+sudo chmod -R 000 ~/Library/LanguageModeling ~/Library/Spelling ~/Library/Suggestions
+sudo chflags -R uchg ~/Library/LanguageModeling ~/Library/Spelling ~/Library/Suggestions
+
+rm -rfv ~/Library/Assistant/SiriAnalytics.db
+
+# ----------------------------------------------------------
+# ---------Deactivate the Remote Management Service---------
+# ----------------------------------------------------------
+echo '--- Deactivate the Remote Management Service'
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -----------Remove Apple Remote Desktop Settings-----------
+# ----------------------------------------------------------
+echo '--- Remove Apple Remote Desktop Settings'
+sudo rm -rf /var/db/RemoteManagement
+sudo defaults delete /Library/Preferences/com.apple.RemoteDesktop.plist
+defaults delete ~/Library/Preferences/com.apple.RemoteDesktop.plist
+sudo rm -r /Library/Application\ Support/Apple/Remote\ Desktop/ 
+rm -r ~/Library/Application\ Support/Remote\ Desktop/
+rm -r ~/Library/Containers/com.apple.RemoteDesktop
+# ----------------------------------------------------------
+
+# ----------------------------------------------------------
+# --------------------Disable "Ask Siri"--------------------
+# ----------------------------------------------------------
+echo '--- Disable "Ask Siri"'
+defaults write com.apple.assistant.support 'Assistant Enabled' -bool false
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------------Disable Siri voice feedback----------------
+# ----------------------------------------------------------
+echo '--- Disable Siri voice feedback'
+defaults write com.apple.assistant.backedup 'Use device speaker for TTS' -int 3
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------Disable Siri services (Siri and assistantd)--------
+# ----------------------------------------------------------
+echo '--- Disable Siri services (Siri and assistantd)'
+launchctl disable "user/$UID/com.apple.assistantd"
+launchctl disable "gui/$UID/com.apple.assistantd"
+sudo launchctl disable 'system/com.apple.assistantd'
+launchctl disable "user/$UID/com.apple.Siri.agent"
+launchctl disable "gui/$UID/com.apple.Siri.agent"
+sudo launchctl disable 'system/com.apple.Siri.agent'
+if [ $(/usr/bin/csrutil status | awk '/status/ {print $5}' | sed 's/\.$//') = "enabled" ]; then
+    >&2 echo 'This script requires SIP to be disabled. Read more: https://developer.apple.com/documentation/security/disabling_and_enabling_system_integrity_protection'
+fi
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------Disable "Do you want to enable Siri?" pop-up-------
+# ----------------------------------------------------------
+echo '--- Disable "Do you want to enable Siri?" pop-up'
+defaults write com.apple.SetupAssistant 'DidSeeSiriSetup' -bool True
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -----------------Hide Siri from menu bar------------------
+# ----------------------------------------------------------
+echo '--- Hide Siri from menu bar'
+defaults write com.apple.systemuiserver 'NSStatusItem Visible Siri' 0
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----------------Hide Siri from status menu----------------
+# ----------------------------------------------------------
+echo '--- Hide Siri from status menu'
+defaults write com.apple.Siri 'StatusMenuVisible' -bool false
+defaults write com.apple.Siri 'UserHasDeclinedEnable' -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ------------Opt-out from Siri data collection-------------
+# ----------------------------------------------------------
+echo '--- Opt-out from Siri data collection'
+defaults write com.apple.assistant.support 'Siri Data Sharing Opt-In Status' -int 2
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------Disable Internet based spell correction----------
+# ----------------------------------------------------------
+echo '--- Disable Internet based spell correction'
+defaults write NSGlobalDomain WebAutomaticSpellingCorrectionEnabled -bool false
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------------Disable Remote Apple Events----------------
+# ----------------------------------------------------------
+echo '--- Disable Remote Apple Events'
+sudo systemsetup -setremoteappleevents off
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----Do not store documents to iCloud Drive by default-----
+# ----------------------------------------------------------
+echo '--- Do not store documents to iCloud Drive by default'
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------------Do not show recent items on dock-------------
+# ----------------------------------------------------------
+echo '--- Do not show recent items on dock'
+defaults write com.apple.dock show-recents -bool false
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------------Disable AirDrop file sharing---------------
+# ----------------------------------------------------------
+echo '--- Disable AirDrop file sharing'
+defaults write com.apple.NetworkBrowser DisableAirDrop -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----------------Disable Spotlight indexing----------------
+# ----------------------------------------------------------
+echo '--- Disable Spotlight indexing'
+sudo mdutil -i off -d /
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------------Enable application firewall----------------
+# ----------------------------------------------------------
+echo '--- Enable application firewall'
+/usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -bool true
+defaults write com.apple.security.firewall EnableFirewall -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -----------------Turn on firewall logging-----------------
+# ----------------------------------------------------------
+echo '--- Turn on firewall logging'
+/usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on
+sudo defaults write /Library/Preferences/com.apple.alf loggingenabled -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------------------Turn on stealth mode-------------------
+# ----------------------------------------------------------
+echo '--- Turn on stealth mode'
+/usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+sudo defaults write /Library/Preferences/com.apple.alf stealthenabled -bool true
+defaults write com.apple.security.firewall EnableStealthMode -bool true
+# ----------------------------------------------------------
+
+
+# Prevent automatically allowing incoming connections to signed apps
+echo '--- Prevent automatically allowing incoming connections to signed apps'
+sudo defaults write /Library/Preferences/com.apple.alf allowsignedenabled -bool false
+# ----------------------------------------------------------
+
+
+# Prevent automatically allowing incoming connections to downloaded signed apps
+echo '--- Prevent automatically allowing incoming connections to downloaded signed apps'
+sudo defaults write /Library/Preferences/com.apple.alf allowdownloadsignedenabled -bool false
+# ----------------------------------------------------------
+
+
+# Require a password to wake the computer from sleep or screen saver
+echo '--- Require a password to wake the computer from sleep or screen saver'
+sudo defaults write /Library/Preferences/com.apple.screensaver askForPassword -bool true
+# ----------------------------------------------------------
+
+
+# Initiate session lock five seconds after screen saver is started
+echo '--- Initiate session lock five seconds after screen saver is started'
+sudo defaults write /Library/Preferences/com.apple.screensaver 'askForPasswordDelay' -int 5
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----Disables signing in as Guest from the login screen----
+# ----------------------------------------------------------
+echo '--- Disables signing in as Guest from the login screen'
+sudo defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool NO
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------Disables Guest access to file shares over AF-------
+# ----------------------------------------------------------
+echo '--- Disables Guest access to file shares over AF'
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ------Disables Guest access to file shares over SMB-------
+# ----------------------------------------------------------
+echo '--- Disables Guest access to file shares over SMB'
+sudo defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -Disable remote login (incoming SSH and SFTP connections)-
+# ----------------------------------------------------------
+echo '--- Disable remote login (incoming SSH and SFTP connections)'
+echo 'yes' | sudo systemsetup -setremotelogin off
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# --------------Disable insecure TFTP service---------------
+# ----------------------------------------------------------
+echo '--- Disable insecure TFTP service'
+sudo launchctl disable 'system/com.apple.tftpd'
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----------Disable Bonjour multicast advertising-----------
+# ----------------------------------------------------------
+echo '--- Disable Bonjour multicast advertising'
+sudo defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------------Disable insecure telnet protocol-------------
+# ----------------------------------------------------------
+echo '--- Disable insecure telnet protocol'
+sudo launchctl disable system/com.apple.telnetd
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# --Disable sharing of local printers with other computers--
+# ----------------------------------------------------------
+echo '--- Disable sharing of local printers with other computers'
+cupsctl --no-share-printers
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -Disable printing from any address including the Internet-
+# ----------------------------------------------------------
+echo '--- Disable printing from any address including the Internet'
+cupsctl --no-remote-any
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----------Disable remote printer administration-----------
+# ----------------------------------------------------------
+echo '--- Disable remote printer administration'
+cupsctl --no-remote-admin
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ------------------Disable Captive portal------------------
+# ----------------------------------------------------------
+echo '--- Disable Captive portal'
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control.plist Active -bool false
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----------------Disable Firefox telemetry-----------------
+# ----------------------------------------------------------
+echo '--- Disable Firefox telemetry'
+# Enable Firefox policies so the telemetry can be configured.
+sudo defaults write /Library/Preferences/org.mozilla.firefox EnterprisePoliciesEnabled -bool TRUE
+# Disable sending usage data
+sudo defaults write /Library/Preferences/org.mozilla.firefox DisableTelemetry -bool TRUE
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----Disable Microsoft Office diagnostics data sending-----
+# ----------------------------------------------------------
+echo '--- Disable Microsoft Office diagnostics data sending'
+defaults write com.microsoft.office DiagnosticDataTypePreference -string ZeroDiagnosticData
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -----------------Uninstall Google update------------------
+# ----------------------------------------------------------
+echo '--- Uninstall Google update'
+googleUpdateFile=~/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Resources/ksinstall
+if [ -f "$googleUpdateFile" ]; then
+    $googleUpdateFile --nuke
+    echo Uninstalled google update
+else
+    echo Google update file does not exist
+fi
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------Disable Homebrew user behavior analytics---------
+# ----------------------------------------------------------
+echo '--- Disable Homebrew user behavior analytics'
+command='export HOMEBREW_NO_ANALYTICS=1'
+declare -a profile_files=("$HOME/.bash_profile" "$HOME/.zprofile")
+for profile_file in "${profile_files[@]}"
+do
+    touch "$profile_file"
+    if ! grep -q "$command" "${profile_file}"; then
+        echo "$command" >> "$profile_file"
+        echo "[$profile_file] Configured"
+    else
+        echo "[$profile_file] No need for any action, already configured"
+    fi
+done
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# --------------Disable NET Core CLI telemetry--------------
+# ----------------------------------------------------------
+echo '--- Disable NET Core CLI telemetry'
+command='export DOTNET_CLI_TELEMETRY_OPTOUT=1'
+declare -a profile_files=("$HOME/.bash_profile" "$HOME/.zprofile")
+for profile_file in "${profile_files[@]}"
+do
+    touch "$profile_file"
+    if ! grep -q "$command" "${profile_file}"; then
+        echo "$command" >> "$profile_file"
+        echo "[$profile_file] Configured"
+    else
+        echo "[$profile_file] No need for any action, already configured"
+    fi
+done
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ------------Disable PowerShell Core telemetry-------------
+# ----------------------------------------------------------
+echo '--- Disable PowerShell Core telemetry'
+command='export POWERSHELL_TELEMETRY_OPTOUT=1'
+declare -a profile_files=("$HOME/.bash_profile" "$HOME/.zprofile")
+for profile_file in "${profile_files[@]}"
+do
+    touch "$profile_file"
+    if ! grep -q "$command" "${profile_file}"; then
+        echo "$command" >> "$profile_file"
+        echo "[$profile_file] Configured"
+    else
+        echo "[$profile_file] No need for any action, already configured"
+    fi
+done
+# ----------------------------------------------------------
+
+
 }
 
 main "$@" 
