3bf02426ed36d9475550338033b5e80bc5558e20
mbpro16m1max <mbpro16m1max@TheOne.local>
2022-08-18 13:12:30 +0300

    Change vscode settings to codium

--- vscode/configure_vscode.sh
+++ vscode/configure_vscode.sh
@@ -8,38 +8,29 @@ main() {
 }
 
 function install_extensions() {
-    code -v > /dev/null
+    codium -v > /dev/null
     if [[ $? -eq 0 ]];then
-        code --install-extension amatiasq.sort-imports
-        code --install-extension bradgashler.htmltagwrap
-        code --install-extension ChakrounAnas.turbo-console-log
-        code --install-extension christian-kohler.npm-intellisense
-        code --install-extension CoenraadS.bracket-pair-colorizer
-        code --install-extension cssho.vscode-svgviewer
-        code --install-extension dbaeumer.vscode-eslint
-        code --install-extension deerawan.vscode-dash
-        code --install-extension donjayamanne.githistory
-        code --install-extension dsznajder.es7-react-js-snippets
-        code --install-extension eamodio.gitlens
-        code --install-extension EditorConfig.EditorConfig
-        code --install-extension esbenp.prettier-vscode
-        code --install-extension formulahendry.auto-close-tag
-        code --install-extension formulahendry.auto-rename-tag
-        code --install-extension miclo.sort-typescript-imports
-        code --install-extension mikestead.dotenv
-        code --install-extension Orta.vscode-jest
-        code --install-extension pflannery.vscode-versionlens
-        code --install-extension steoates.autoimport
-        code --install-extension streetsidesoftware.code-spell-checker
-        code --install-extension stylelint.vscode-stylelint
-        code --install-extension whtouche.vscode-js-console-utils
-        code --install-extension yzhang.markdown-all-in-one
-        code --install-extension ZainChen.json
+        codium --install-extension formulahendry.auto-close-tag
+        codium --install-extension steoates.autoimport
+        codium --install-extension redwan-hossain.auto-rename-tag-clone
+        codium --install-extension streetsidesoftware.code-spell-checker
+        codium --install-extension mikestead.dotenv
+        codium --install-extension EditorConfig.EditorConfig
+        codium --install-extension dsznajder.es7-react-js-snippets
+        codium --install-extension dbaeumer.vscode-eslint
+        codium --install-extension donjayamanne.githistory
+        codium --install-extension Orta.vscode-jest
+        codium --install-extension yzhang.markdown-all-in-one
+        codium --install-extension christian-kohler.npm-intellisense
+        codium --install-extension esbenp.prettier-vscode
+        codium --install-extension stylelint.vscode-stylelint
+        codium --install-extension pflannery.vscode-versionlens
+        
         fi
 }
 
 function configure_settings() {
-    cp ./settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+    cp ./settings.json $HOME/Library/Application\ Support/VSCodium/User/settings.json
 }
 
 main "$@"
